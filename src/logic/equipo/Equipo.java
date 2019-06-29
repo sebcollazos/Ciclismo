0f211d7cc9c6f7bfd8dbb2d2ad3086146f02bc64
secoveGordo <sebcollazos@unicauca.edu.co>
2019-06-29 18:28:01 -0500

    Merge branch 'master' of https://github.com/secoveGordo/Ciclismo
    # Conflicts:
    #	src/logic/puntuacion/Tiempo.java

--- src/logic/equipo/Equipo.java
+++ src/logic/equipo/Equipo.java
@@ -3,7 +3,7 @@ import logic.ciclista.Ciclista;
 import java.util.ArrayList;
 
 public class Equipo {
-    private ArrayList<Ciclista> cicistas;
+    private ArrayList<Ciclista> ciclistas;
     private String nombre;
     private String codigo;
 
@@ -12,29 +12,41 @@ public class Equipo {
     }
 
     public Equipo(ArrayList<Ciclista> cicistas, String nombre, String codigo) {
-        this.cicistas = cicistas;
+        this.ciclistas = cicistas;
         this.nombre = nombre;
         this.codigo = codigo;
     }
 
     public ArrayList<Ciclista> getCicistas() {
-        return cicistas;
+        return ciclistas;
     }
-    public void setCicistas(ArrayList<Ciclista> cicistas) {
-        this.cicistas = cicistas;
+
+    public void setCicistas(ArrayList<Ciclista> ciclistas) {
+        this.ciclistas = ciclistas;
     }
+
     public String getCodigo() {
         return codigo;
     }
+
     public void generarCodigo() {
-        this.codigo = this.nombre.charAt(0) + this.nombre.charAt(1) + this.nombre.charAt(2);
+
+        String a= this.nombre;
+
+        String[] c = a.split("");
+
+        this.codigo= c[0] + c[1] + c[2];
+
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public boolean addCiclista(Ciclista c) {
         for (int i = 0; i<= this.ciclistas.size(); i++) {
             if (this.ciclistas.get(i).getNombre().equals(c.getNombre())){
--- src/logic/uci/UCI.java
+++ src/logic/uci/UCI.java
@@ -26,24 +26,60 @@ public class UCI {
 
     }
 
-    public ArrayList<Double> calcularPuntosMontaña(Double aF, DOuble aI, Double Km){
+    public ArrayList<Double> calcularPuntosMontania(Double aF, Double aI, Double Km){
 
         Double gradoMontaña= ((aF-aI)/Km) *  100;
-        String categoria= " ";
+        ArrayList<Double> categoria= new ArrayList<>();
 
         if(gradoMontaña >= 140 && gradoMontaña <250){
-            categoria= "Especial";
+
+            Double p= 20.0;
+
+            for(int i=0 ; i < 6; i++){
+                if(p> 4){
+                    categoria.add(i, p);
+                    p= p-4;
+                }else{
+                    categoria.add(i, 2.0);
+                }
+            }
+
         }else if(gradoMontaña >= 80 && gradoMontaña <140){
-            categoria= "1ra";
+
+            Double p= 10.0;
+
+            for(int i=0 ; i < 6; i++){
+                if(p> 4){
+                    categoria.add(i, p);
+                    p= p-2;
+                }else{
+                    categoria.add(i, 1.0);
+                }
+            }
+
         }else if(gradoMontaña >= 50 && gradoMontaña <80){
-            categoria= "2da";
+            Double p= 5.0;
+            Double b= 1.0;
+
+            for(int i=0 ; i < 6; i++){
+                b= b + 1.0;
+                categoria.add(i, p);
+                p= p-b;
+            }
         }else if(gradoMontaña >= 30 && gradoMontaña <50){
-            categoria= "3ra";
+            Double p= 2.0;
+
+            for(int i=0 ; i < 1; i++){
+                categoria.add(i, p);
+                p= p-1.0;
+            }
         }else if(gradoMontaña >= 20 && gradoMontaña <30){
-            categoria= "4ta";
+            categoria.add(0, 1.0);
         }
 
-        return null;
+
+
+        return categoria;
 
     }
 }
