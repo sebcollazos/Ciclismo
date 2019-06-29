2515369c3f3a1142087a8e42b9c117428ee8b79e
DavidJimG <david993110@gmail.com>
2019-06-29 17:11:35 -0500

    Al fin

--- src/logic/puntuacion/Tiempo.java
+++ src/logic/puntuacion/Tiempo.java
@@ -12,10 +12,10 @@ public class Tiempo {
 
 
 
-    public Tiempo(){
+    public Tiempo() {
 
     }
-    public Tiempo(int horas, int minutos, int segundos){
+    public Tiempo(int horas, int minutos, int segundos) {
         this.horas = horas;
         this.minutos = minutos;
         this.segundos = segundos;
@@ -23,48 +23,38 @@ public class Tiempo {
 
 
 
-    public int getHoras() {
-        return this.horas;
-    }
-    public void setHoras(int horas) {
-        this.horas = horas;
-    }
-    public int getMinutos() {
-        return this.minutos;
-    }
-    public void setMinutos(int minutos) {
-        this.minutos = minutos;
-    }
-    public int getSegundos() {
-        return this.segundos;
-    }
-    public void setSegundos(int segundos) {
-        this.segundos = segundos;
-    }
+    public int getHoras() { return horas; }
+    public void setHoras(int horas) { this.horas = horas; }
+    public int getMinutos() { return minutos; }
+    public void setMinutos(int minutos) { this.minutos = minutos; }
+    public int getSegundos() { return segundos; }
+    public void setSegundos(int segundos) { this.segundos = segundos; }
 
 
 
-
-    public Tiempo calcularPuntos(Tiempo t, String op){
-        if(op.equals("+")){
-            int tmp1 = t.getSegundos() + this.segundos;
-            if (tmp1 >= 60){
-                this.segundos = tmp1 - 60;
-                this.minutos = this.minutos + 1;
+    public Tiempo calcularTiempo(Tiempo t, String op) {
+        Tiempo tm = new Tiempo(this.getHoras(), this.getMinutos(), this.getSegundos());
+        if (op.equals("+")) {
+            int tmp1 = t.getSegundos() + tm.getSegundos();
+            if (tmp1 >= 60) {
+                tm.setSegundos(tmp1 - 60);
+                tm.setMinutos(tm.getMinutos() + 1);
             }
-            else{
-                this.segundos = tmp1;
+            else {
+                tm.setSegundos(tmp1);
             }
-            int tmp2 = minutos + this.minutos;
-            if (tmp2 >= 60){
-                this.minutos = tmp2 - 60;
-                this.horas = this.horas + 1;
+            int tmp2 = t.getMinutos() + tm.getSegundos();
+            if (tmp2 >= 60) {
+                tm.setMinutos(tmp2 - 60);
+                tm.setHoras(tm.getHoras() + 1);
             }
-            else{
-                this.minutos = tmp2;
+            else {
+                tm.setMinutos(tmp2);
             }
-            this.horas = this.horas + horas;
+            tm.setHoras(tm.getHoras() + horas);
         }
+        return tm;
     }
-
 }
+
+
